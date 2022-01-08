5fd3bc2dcf84fdac855bfa5cf3b592846daf46d6
Hayden Eagle <haydeneagle@gmail.com>
2022-01-08 18:52:55 +1100

    add initial

---
+++ tank-monitoring/src/main.cpp
@@ -0,0 +1,41 @@
+#include <Arduino.h>
+
+
+#define trigPin 22
+#define echoPin 21
+
+// Define variables:
+long duration;
+int distance;
+
+void setup() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  digitalWrite(trigPin, LOW);
+  
+  delayMicroseconds(5);
+
+ // Trigger the sensor by setting the trigPin high for 10 microseconds:
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  // Read the echoPin. pulseIn() returns the duration (length of the pulse) in microseconds:
+  duration = pulseIn(echoPin, HIGH);
+  
+  // Calculate the distance:
+  distance = duration*0.034/2;
+  
+  // Print the distance on the Serial Monitor (Ctrl+Shift+M):
+  Serial.print("Distance = ");
+  Serial.print(distance);
+  Serial.println(" cm");
+  
+  delay(100);
+}
